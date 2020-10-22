2e375ba2ba83aee3d5dd2d538c40f085f5cdfd8f
Delsin Van Grembergen <delsin.vangrembergen@student.odisee.be>
2020-10-22 19:19:31 +0200

    Routes + RecipeController

---
+++ recipes/app/Http/Controllers/RecipeController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RecipeController extends Controller
+{
+    public function index() {
+        return view('recipes');
+    }
+
+    public function show() {
+        return view('welcome');
+    }
+}
--- recipes/routes/web.php
+++ recipes/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\RecipeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/recipes');
 });
+
+Route::get('/recipes', [RecipeController::class, 'index']);
+Route::get('/recipe/{id}', [RecipeController::class, 'show']);
