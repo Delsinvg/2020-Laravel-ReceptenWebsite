9595fa66c88cbf2b531b73ab4a357b9ddde23d01
Delsin Van Grembergen <delsin.vangrembergen@student.odisee.be>
2021-02-21 10:22:26 +0100

    Updating packages

--- recipes/app/Http/Controllers/RecipeController.php
+++ recipes/app/Http/Controllers/RecipeController.php
@@ -10,15 +10,26 @@ use App\Models\RecipeIngredients;
 
 class RecipeController extends Controller
 {
-    public function index() {
-        $recipes = Recipe::where('visible', true)->simplePaginate(6);
+    public function index()
+    {
+        $recipes = Recipe::where('visible', true)
+            ->simplePaginate(6);
         return view('recipes', ['recipes' => $recipes]);
     }
 
-    public function show($id) {
-        $recipe = Recipe::with('ingredient')->where('visible', true)->findOrFail($id);
+    public function show($id)
+    {
+        $recipe = Recipe::with('ingredient')
+            ->where('visible', true)
+            ->findOrFail($id);
         $categoryId = $recipe->category->id;
-        $randomRecipes = Recipe::where('id', '!=', $id)->where('categoryId', $categoryId)->where('visible', true)->distinct('name')->inRandomOrder()->limit(4)->get();
+        $randomRecipes = Recipe::where('id', '!=', $id)
+            ->where('categoryId', $categoryId)
+            ->where('visible', true)
+            ->distinct('name')
+            ->inRandomOrder()
+            ->limit(4)
+            ->get();
         return view('recipedetails', ['recipe' => $recipe, 'randomRecipes' => $randomRecipes]);
     }
 }
